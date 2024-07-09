eda0ac5f71b5c6efd8d9f513746c9db6d1e16778
GFO <gia.focxverashvili@gmail.com>
2024-07-09 20:50:45 +0400

    folder name changes

--- UNIT 2 - Spring/Assignments14/src/calculator/Calculator.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-
-import shapes.Shape;
-
-public class Calculator {
-   public double calculateArea(Shape shape){
-       return shape.calculateArea();
-    }
-}
--- UNIT 2 - Spring/Assignments14/src/display/ShapeInformation.java
+++
@@ -1,8 +0,0 @@
-package display;
-
-import shapes.Shape;
-public class ShapeInformation {
-    public  String getShapeType(Shape shape) {
-        return shape.getClass().getSimpleName();
-    }
-}
