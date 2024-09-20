98097ade39787d25047aaf60612a38afb20c5762
GFO <gia.focxverashvili@gmail.com>
2024-09-20 22:09:35 +0400

    news

---
+++ UNIT 4 - WEB/GroupAssignment/src/test/java/com/example/GroupAssignment/GroupAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GroupAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
