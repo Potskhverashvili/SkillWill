fb6ed649b994e9aa4ddaa723b51cdd7dfecfb824
GFO <gia.focxverashvili@gmail.com>
2025-01-03 11:19:23 +0400

    final commit

---
+++ UNIT 6 - Multi-Threading/Assignments35-AOP/src/main/java/com/example/Assignments35_AOP/Assignments35AopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Assignments35_AOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Assignments35AopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Assignments35AopApplication.class, args);
+    }
+}
